a5f78d462d7e00af4ddc7eabf355797cd64cba55
Alex Woodman <alexthegoodman@gmail.com>
2021-02-03 19:18:33 -0500

    temp type fix

--- ErrorHandler.ts
+++ ErrorHandler.ts
@@ -1,6 +1,8 @@
 export const ErrorHandler = (
-  error: Error,
-  info: { componentStack: string }
+  // error: Error,
+  // info: { componentStack: string }
+  error,
+  info
 ) => {
   console.error(`Error: `, error, ` ComponentStack: `, info.componentStack);
   // rollbar
